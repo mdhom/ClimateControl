f07b6802f66d9b4a642086b67458bb65a413d99a
mdhom <max@dhom.info>
2021-03-11 19:42:54 +0100

    Config Leo

--- src/config.h
+++ src/config.h
@@ -2,7 +2,7 @@
 #include <PubSubClient.h>
 #include <ArduinoJson.h>
 
-const char* roomIdentifier              = "Dachboden";
+const char* roomIdentifier              = "kitchen";
 const char* mqttTopic                   = "airControl";
 
 const char* mqttWriteTopicSuffix        = "write";
@@ -15,14 +15,15 @@ const char* mqttErrorSuffix             = "error";
 #define STORE_IN_FLASH
 
 /* MQTT Settings */
-IPAddress broker(192,168,188,149);                     // Address of the MQTT broker
+//IPAddress broker(192,168,188,149); // MQTT Broker Max
+IPAddress broker(192,168,178,78); // MQTT Broker Leo
 //#define USE_MQTT_AUTH
 //#define MQTT_USER "USERNAME"
 //#define MQTT_PASSWORD "PASSWORD"
 
 /* WiFi Settings */
-const char* ssid     = "MMD";
-const char* password = "MMD%83139%";
+const char* ssid     = "heenalu 2,4";
+const char* password = "86%FQcOXI!3L";
 
 const int NumBME = 4;
 /* Sensor Settings */
